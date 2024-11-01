6040bbd3a2a4089279866effdccbd5cae6ea64bf
SKY1GER <saikumarb9119@gmail.com>
2024-11-01 22:10:41 +0530

    working

--- mysql.sh
+++ mysql.sh
@@ -42,11 +42,11 @@ VALIDATE $? "staring mysql-server"
 # mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$logfile
 # VALIDATE $? "setting up root password for mysql-server"
 
-mysql -h db.daws79s.online -uroot -p$my_sql_password -e 'SHOW DATABASES;'
+mysql -h db.daws79s.online -uroot -p${my_sql_password} -e 'SHOW DATABASES;'
 if [ $? -ne 0 ]
 then
     echo -e "$Y *** installing My Sql *** $N"
-    mysql_secure_installation --set-root-pass $my_sql_password &>>$logfile
+    mysql_secure_installation --set-root-pass ${my_sql_password} &>>$logfile
     VALIDATE $? "setting up root password for mysql-server"
 else
     echo -e "$G My Sql root password already setup $Y *** Skipping *** $N"

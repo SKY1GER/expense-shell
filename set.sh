39a4fddb2c39f11d62c6dfcc1490cbb12cc1ff48
SKY1GER <saikumarb9119@gmail.com>
2024-11-02 21:43:21 +0530

    Complete expense app automatic configuration

---
+++ set.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+handle_error(){
+   echo "error occured at line no. $1 : error comand is $2"
+}
+trap "handle_error ${LINENO} "$BASH_COMMAND"' ERR
+
+source ./common.sh
+
+check_root
+
+echo "please enter mysql password"
+read -s my_sql_password
+
+dnf install rfyhyvg -y &>>$logfile #script will exit here because of set -e
+VALIDATE $? "$G installed nodejs:20 $N"

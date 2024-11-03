380ba0ec0dd8f6d8ae33f3912fde29b8cd87805f
SKY1GER <saikumarb9119@gmail.com>
2024-11-03 11:41:38 +0530

    Complete expense app automatic configuration

--- discmemory.sh
+++ discmemory.sh
@@ -8,7 +8,7 @@ Message=''
  do
     usage=$(echo $line | awk -F "" '{print $6F}' | cut -d "%" -f1)
     folder=$(echo $line | awk -F "" '{print $NF}')
-    if [ $usage -ge $Threshold]
+    if [ $usage -ge $Threshold ]
     then
     Message+="$folder is more than $Threshold, current usage :$usage"
     fi
